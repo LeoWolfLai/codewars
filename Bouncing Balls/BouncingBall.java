d42199d6a7037ab25f93e65c0313d895afc30eab
Leo <leo.lai.wolf@gmail.com>
2017-02-10 14:07:10 +0800

    Update BouncingBall.java

--- Bouncing Balls/BouncingBall.java
+++ Bouncing Balls/BouncingBall.java
@@ -1,14 +1,12 @@
 public class BouncingBall {
 
 	public static int bouncingBall(double h, double bounce, double window) {
-	  if(bounce <=0 || 1<=bounce || window>=h)
-      return -1;
+		if(bounce <=0 || 1<=bounce || window>=h)
+			return -1;
 
-    int result = 1;
-    for(double height=h*bounce; height>=window; height*=bounce){
-      result+=2;
-    }
-
+		int result = 1;
+		for(double height = h*bounce; height>=window; height*=bounce)
+			result+=2;
     return result;
-	}
+  }
 }
